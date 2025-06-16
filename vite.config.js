e594affa46f92f564da28440ed6fafc6ac0c93ed
INGANZAMARUMPU <jonkur96@gmail.com>
2025-06-16 10:34:13 +0200

    vite

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,19 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), tailwindcss()],
   build: {
-    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: () => 'everything.js', // Prevent code splitting
+        entryFileNames: `assets/[name].js`,
+        chunkFileNames: `assets/[name].js`,
+        assetFileNames: `assets/[name].[ext]`,
+      },
+    },
+    minify: true,
+    cssCodeSplit: false, // Bundle CSS into JS
+    outDir: 'dist', // Output directory
   },
-  server: {
-    host: '0.0.0.0',
-    port: 5173
-  }
 })
